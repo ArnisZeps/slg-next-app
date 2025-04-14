1a07b4ad10d7eda1d9d641194a8c6f435257f22e
Arnis Zeps <arnis.zeps@1nce.com>
2025-04-14 22:41:22 +0300

    refactor: component reorganization under src

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,17 +3,20 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 // import Image from "next/image";
 import ContactForm from "./_components/ContactForm/ContactForm";
+import { useTranslations } from "next-intl";
 
 export default function Home() {
+  const t = useTranslations();
+  console.log(t)
   return (
     <div className="flex flex-col ">
       {/* Full-page height div */}
       <section className="h-screen flex flex-col snap-start">
         <Header />
-        <div className="flex-1 flex flex-col items-center justify-center bg-gradient-to-r from-violet-700 to-sky-600">
-          <div className="flex flex-col gap-6 items-center  md:gap-12 text-white">
-            <h1 className="text-2xl">Professional resume builder</h1>
-            <div className="max-w-96 font-medium border-l-4 border-violet-900 pl-4 space-y-2 md:max-w-full">
+        <div style={{ backgroundImage: "url('/classic-hero.jpg')"}} className="bg-cover flex-1 flex flex-col items-center justify-center">
+          <div className="bg-red-300/85 p-14 flex flex-col gap-6 items-center  md:gap-12 text-white rounded-md">
+            <h1 className="text-2xl">Profesionāls risinājums tavām mājām</h1>
+            <div className="max-w-96 font-medium pl-4 space-y-2 md:max-w-full">
               <p>Build best looking resume in minutes!</p>
               <p>Only 2% of resumes win. Yours will be one of them. Lets build you a resume that works.</p>
               <p>Use best industry templates</p>
