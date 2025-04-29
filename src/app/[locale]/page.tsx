d3d8a6bd3a9079368538b3582e775188ef1aa322
Arnis Zeps <zeps.arnis@gmail.com>
2025-04-29 16:14:33 +0300

    fix: fixed shape appearance

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -15,19 +15,19 @@ export default function Home() {
         <Header />
         <div
           style={{ backgroundImage: "url('/classic-hero.jpg')" }}
-          className="relative bg-cover bg-center flex flex-1 flex-col justify-center md:items-start -z-20"
+          className="bg-cover bg-center flex flex-1 flex-col justify-center md:items-start -z-20"
         >
-          <img src="/shape_1.svg" alt="Decorative Shape" className="absolute top-0 left-0 w-full h-full object-cover -z-10 opacity-60" />
-          <div className="font-bold text-white rounded-md flex flex-col gap-6 items-center p-8 md:p-14 md:gap-12 md:max-w-3/7 ">
-            <h1 className="text-3xl">{t("HeroTitle")}</h1>
-            {/* Desktop Version */}
-            <div className="md:block text-lg pl-4 space-y-2 md:max-w-full">
-              <h1 className="text-2xl font-extrabold">{t("HeroBulletP1Title")}</h1>
-              <p>{t("HeroBulletP1")}</p>
-            </div>
+          <div style={{ backgroundImage: "url('/shape_1.svg')" }} className="relative bg-cover opacity-80 flex flex-1 flex-col justify-center md:items-start">
+            <div className="font-bold text-white rounded-md flex flex-col gap-6 items-center p-8 md:p-14 md:gap-12 md:max-w-3/7 ">
+              <h1 className="text-3xl">{t("HeroTitle")}</h1>
+              {/* Desktop Version */}
+              <div className="md:block text-lg pl-4 space-y-2 md:max-w-full">
+                <h1 className="text-2xl font-extrabold">{t("HeroBulletP1Title")}</h1>
+                <p>{t("HeroBulletP1")}</p>
+              </div>
 
-            {/* Mobile Accordion */}
-            {/* <div className="md:hidden w-full">
+              {/* Mobile Accordion */}
+              {/* <div className="md:hidden w-full">
               <Accordion
                 items={[
                   { title: t("HeroBulletP1Title"), content: t("HeroBulletP1") },
@@ -37,13 +37,15 @@ export default function Home() {
                 ]}
               />
             </div> */}
+            </div>
           </div>
+          {/* <img src="/shape_1.svg" alt="Decorative Shape" className="absolute top-0 left-0 w-full h-full object-cover -z-10 opacity-60" /> */}
         </div>
       </section>
       <section className="min-h-screen flex flex-col snap-start ">
         <div className="relative flex flex-col flex-1 items-center gap-10 p-8 md:justify-between md:flex-row md:p-14">
           <div className="relative aspect-square w-full md:w-3/7 md:max-w-2xl ">
-            <Image alt="skirting" fill src="/salag_1.jpg" className="rounded-md"/>
+            <Image alt="skirting" fill src="/salag_1.jpg" className="rounded-md" />
           </div>
           <div className="flex flex-col md:max-w-3/7">
             <h1 className="text-xl font-extrabold">{t("HeroBulletP2Title")}</h1>
